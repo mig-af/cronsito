9f82b6d11dbae9241b1a1b72f4a987baa2ec72a7
mig-af <mkmouse786@gmail.com>
2025-06-25 23:02:28 +0000

    siu

---
+++ cronsito.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+
+
+
+
+apik=os.environ.get("TELEGRAM")
+url = "https://api.telegram.org/bot"+apik
+def get(metthod):
+    message = {
+        "chat_id":1462438140,
+        "text":"hello"
+    }
+
+    req = url+metthod
+    resp = requests.get(req, data=message)
+    print(resp.text)
+    return True, 200
+
+get("/sendMessage")
+
